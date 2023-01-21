caac73f68ab8236e5112515a0a638c1eac35db41
Samuel Patiño <levivo2@hotmail.com>
2023-01-21 01:26:00 -0300

    Clase 16 React Context - Estado Compartido - 1 - obtenemos las propiedades desde AppUI envueltas en el todoProvider

--- src/App/index.js
+++ src/App/index.js
@@ -9,64 +9,20 @@ import React from "react";
 
 import { AppUI } from './AppUI';
 
+import { TodoProvider } from "../TodoContext/";
 // const defaultTodos = [
 //   { text : 'Cortar cebolla', completed: true },
 //   { text : 'Cortar Papas', completed: true },
 //   { text : 'Cortar Tomates', completed: false }
 // ]
-
  
   function App() {
-    // Desestructuramos los nuevos datos de nustro custom hook
-    const {
-      item: todos,
-      saveItem: saveTodos,
-      loading,
-      error,
-    } = useLocalStorage('TODOS_V1', []);
-    const [searchValue, setSearchValue] = React.useState('');
-  
-    const completedTodos = todos.filter(todo => !!todo.completed).length;
-    const totalTodos = todos.length;
-  
-    let searchedTodos = [];
-  
-    if (!searchValue.length >= 1) {
-      searchedTodos = todos;
-    } else {
-      searchedTodos = todos.filter(todo => {
-        const todoText = todo.text.toLowerCase();
-        const searchText = searchValue.toLowerCase();
-        return todoText.includes(searchText);
-      });
-    }
-  
-    const completeTodo = (text) => {
-      const todoIndex = todos.findIndex(todo => todo.text === text);
-      const newTodos = [...todos];
-      newTodos[todoIndex].completed = true;
-      saveTodos(newTodos);
-    };
-  
-    const deleteTodo = (text) => {
-      const todoIndex = todos.findIndex(todo => todo.text === text);
-      const newTodos = [...todos];
-      newTodos.splice(todoIndex, 1);
-      saveTodos(newTodos);
-    };
-  
+   
     return (
-      <AppUI
-        loading={loading}
-        error={error}
-        totalTodos={totalTodos}
-        completedTodos={completedTodos}
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-        searchedTodos={searchedTodos}
-        completeTodo={completeTodo}
-        deleteTodo={deleteTodo}
-      />
+      // obtenemos las propiedades desde AppUI envueltas en el todoProvider
+      <TodoProvider>
+        <AppUI />
+      </TodoProvider>
     );
   }
   
