53a8200391ea3a380d82874fa81455ad399bbfd0
Samuel Patiño <levivo2@hotmail.com>
2023-01-21 00:32:42 -0300

    Clase 16 React Context - Estado Compartido - 1 - Movemos la funcion useLocalStorage a un nuevo archivo useLocalStorage.js

---
+++ src/TodoContext/indes.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TodoContext = React.createContext();
+
+function TodoProvider(props){
+    return(
+        <TodoContext.Provider value={{
+
+        }}>
+            {props.children}
+        </TodoContext.Provider>
+    );
+}
+
+<TodoContext.Consumer></TodoContext.Consumer>
