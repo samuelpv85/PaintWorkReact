f5399d6377234d1065947fa49a85f3a131e22056
Samuel Patiño <levivo2@hotmail.com>
2023-01-22 00:52:43 -0300

    Clase 18 Modal, Activar y desactivar modal en el boton +

--- src/Components/CreateTodoButton/index.js
+++ src/Components/CreateTodoButton/index.js
@@ -3,7 +3,7 @@ import '../css/style.css'
 
 function CreateTodoButton(props) {
   const onClickButton = () => {
-    props.setOpenModal(true)
+    props.setOpenModal(prevState => !prevState);
   };
 
   return (
